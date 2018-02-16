719843764a63751317910a649e5cd01f1b7e6eec
Formation <Formation@M2i-JAV5-04>
2018-02-16 11:24:22 +0100

    Formation no01 TEST2

---
+++ src/atelierjava01/exercice_ferme/Fermier.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjava01.exercice_ferme;
+
+/**
+ *
+ * @author Formation
+ */
+public class Fermier {
+    
+    private String nom,prenom;
+    
+}
