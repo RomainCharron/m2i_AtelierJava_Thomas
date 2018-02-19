2574563cf7759cbe2bf5ee469d9ac500be33e05c
Formation <Formation@M2i-JAV5-04>
2018-02-19 16:57:45 +0100

    Formation no01 démarage de la ferme

---
+++ src/atelierjava01/exercice_ferme/entite/Ressource.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjava01.exercice_ferme.entite;
+
+import javax.print.DocFlavor;
+
+/**
+ *
+ * @author Formation
+ */
+public class Ressource {
+    
+    public enum TypeRessource{
+        ANIMAL_MOUNTON,
+        ANIMAL_VACHE,
+        CULTURE_BLE,
+        CULTURE_MAIS,
+        FERMIER
+    }
+    
+    private String nom;
+    private TypeRessource laDesignation;
+
+    public Ressource(String nom, TypeRessource laDesignation) {
+        this.nom = nom;
+        this.laDesignation = laDesignation;
+    }
+
+    
+
+    public void setLaDesignation(TypeRessource laDesignation) {
+        this.laDesignation = laDesignation;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public TypeRessource getLaDesignation() {
+        return laDesignation;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    
+}
---
+++ src/atelierjava01/exercice_ferme/entite/Terrain.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjava01.exercice_ferme.entite;
+
+//import java.util.*;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Formation
+ */
+public class Terrain {
+    
+    private double TailleHectare;
+    private ArrayList<Ressource> listRessource = new ArrayList<>();
+
+    public Terrain(double TailleHectare) {
+        this.TailleHectare = TailleHectare;
+    }
+
+    public ArrayList<Ressource> getListRessource() {
+        return listRessource;
+    }
+
+    public double getTailleHectare() {
+        return TailleHectare;
+    }
+
+    public void setListRessource(ArrayList<Ressource> listRessource) {
+        this.listRessource = listRessource;
+    }
+
+    public void setTailleHectare(double TailleHectare) {
+        this.TailleHectare = TailleHectare;
+    }
+    
+    
+            
+}
