51c4c3c9d7842bcd26d14053eaadf83b268b14df
Formation <Formation@M2i-JAV5-04>
2018-02-22 17:04:42 +0100

    Formation no07 DAO JPA

---
+++ test/AtelierJAVA01/exercice_ferme/entite/test/JPATest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AtelierJAVA01.exercice_ferme.entite.test;
+
+
+import atelierjava01.exercice_ferme.dao.JoueurDAO;
+import atelierjava01.exercice_ferme.entiti.Joueur;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author Formation
+ */
+public class JPATest {
+    
+    @Test
+    public void runJPA_OK() {
+        
+        //Persistence unePersistence = new Persistence();
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Charron");
+        EntityManager jPAEM = factory.createEntityManager();
+        jPAEM.getTransaction().begin();
+        
+        Joueur unJoueur = new Joueur("Marco", "Polo55", 100d);//Joueur(Double.NaN, "Marco", "polo");
+        
+        jPAEM.persist(unJoueur);
+        
+        jPAEM.getTransaction().commit();
+    }
+    
+    @Test
+    public boolean select01_OK(){
+    
+        EntityManager em = Persistence.createEntityManagerFactory("PU_Charron").createEntityManager();
+        
+        Query qQuery = em.createQuery(""
+                + "SELECT j FROM Joueur j "
+                + "WHERE j.nom = :pseudo "
+                + "AND j.motdepasse = :password");
+        qQuery.setParameter("password", "Marco");
+        qQuery.setParameter("pseudo", "Polo55");
+        
+        return (Joueur)qQuery.getSingleResult() != null; 
+    }
+}
